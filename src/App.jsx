74d4e2cb482c1e971a45379b9ade6f92680177ad
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2024-06-25 20:10:20 -0700

    Created missed components. Imported and replaced all Text to their corresponding components to complete the basic structure of the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
 import styles from './styles';
+import {  Navbar, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonials, Hero } from './components';
 
 // Can remove {} if the component only returns JSX
 const App = () => (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>  {/* styles allows to CRTL Click to see the CSS in the styles.js file*/}
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar />
         </div>
       </div>
       {/* Hero Section: Main part: Called Hero as it is the landing part: Everything that is visible as soon as the page is loaded */}
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CardDeal
-          Testimonials
-          Clients
-          CTA
-          Footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
       </div>
 
